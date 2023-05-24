c46b5f703b272a48d5102cea7b3c51f044a76295
Mark McArdle <mark.mcardle@sohonet.com>
2023-05-24 09:53:21 +0100

    Django 4.2

--- selectable/__init__.py
+++ selectable/__init__.py
@@ -3,4 +3,3 @@
 
 __version__ = '1.3.0'
 
-default_app_config = 'selectable.apps.SelectableConfig'
--- selectable/tests/urls.py
+++ selectable/tests/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import handler404, handler500, include, url
+from django.urls import include, path
+from django.conf.urls import handler404, handler500
 
 
 handler404 = 'selectable.tests.views.test_404'
 handler500 = 'selectable.tests.views.test_500'
 
 urlpatterns = [
-    url(r'^selectable-tests/', include('selectable.urls')),
+    path('selectable-tests/', include('selectable.urls')),
 ]
